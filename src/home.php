153e13e78ca1d0ae7fe9c3fbe1b29f130853ba89
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-07-20 23:00:55 +0200

    cards

--- src/home.php
+++ src/home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>LoveLol</title>
 </head>
 <body>
     <header id="header">
@@ -77,8 +77,6 @@
                 </div>
             </div>
 
-
-           
         
         <script src="./home.js"></script>
     </main>
