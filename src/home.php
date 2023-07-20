f1cd76939b417f5d8d4bf1e933188a759d346cec
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-07-20 22:55:18 +0200

    cards

---
+++ src/home.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header id="header">
+        <div class="header__container">
+            <img src="./img/LOGO.png" alt="logo-LoveLol">
+            <img src="image.png" alt="title-img">
+        </div>
+    </header>
+
+    <main id="main">
+        <h1 class="main__title">Product</h1>
+
+        <div class="main__post">
+            <div class="main__img-container">
+                <img src="./img/gorroporo (1).png" alt="img-product">
+            </div>
+            <div class="main__text-container">
+                <h3>Poro Hat</h3>
+                <h3>23$</h3>
+            </div>
+            <div class= "main__buttons">
+                <button class="main__add-button">Add to cart</button>
+                <button class="main__heart-button" type="button"></button>
+            </div>
+            </div>
+
+            <div class="main__post">
+                <div class="main__img-container">
+                    <img src="./img/pulseraekko.png" alt="img-product">
+                </div>
+                <div class="main__text-container">
+                    <h3>Pulsera de Ekko
+                    </h3>
+                    <h3>47€</h3>
+                </div>
+                <div class= "main__buttons">
+                    <button class="main__add-button">Add to cart</button>
+                    <button class="main__heart-button" type="button"></button>
+                </div>
+            </div>
+
+            <div class="main__post">
+                <div class="main__img-container">
+                    <img src="./img/relojinx.png" alt="img-product">
+                </div>
+                <div class="main__text-container">
+                    <h3>Reloj Jinx (Digital)
+                    </h3>
+                    <h3>43€</h3>
+                </div>
+                <div class= "main__buttons">
+                    <button class="main__add-button">Add to cart</button>
+                    <button class="main__heart-button" type="button"></button>
+                </div>
+            </div>
+
+            <div class="main__post">
+                <div class="main__img-container">
+                    <img src="./img/figurasera.png" alt="img-product">
+                </div>
+                <div class="main__text-container">
+                    <h3>Figura Seraphine
+                    </h3>
+                    <h3>80€</h3>
+                </div>
+                <div class= "main__buttons">
+                    <button class="main__add-button">Add to cart</button>
+                    <button class="main__heart-button" type="button"></button>
+                </div>
+            </div>
+
+
+           
+        
+        <script src="./home.js"></script>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
